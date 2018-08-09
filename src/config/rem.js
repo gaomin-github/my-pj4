bd4e61a02ff86f6a6abd97094dece6b931745f89
GMfight <374204501@qq.com>
2018-08-09 23:08:47 +0800

    customplugin
    自定义的js插件

---
+++ src/config/rem.js
@@ -0,0 +1,25 @@
+export default{
+  //存储字体值
+  fontSizeNum:0,
+  //重置页面默认字体
+  fontSizeReset:function(){
+    window.addEventListener('DOMContentLoaded',()=>{
+      let clientX=document.body.clientWidth
+      document.documentElement.style.fontSize=(clientX/320)*12+'px'
+
+      this.fontSizeNum=clientX/320
+    })
+    window.addEventListener('resize',()=>{
+      let clientX=document.body.clientWidth
+      document.documentElement.style.fontSize=(clientX/320)*12+'px'
+      this.fontSizeNum=clientX/320
+    })
+  },
+  //监听页面垂直滚动值
+  scrollY:function(){
+    window.addEventListener('scroll',()=>{
+      let scrollTopN=document.documentElement.scrollTop||document.body.scrollTop
+      return scrollTopN;
+    })
+  }
+}
