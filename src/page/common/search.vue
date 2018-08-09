d2cd7793bf9443cfabaf4bca8e620d5dc5c0bf92
GMfight <374204501@qq.com>
2018-08-09 23:10:10 +0800

    search
    查询

---
+++ src/page/common/search.vue
@@ -0,0 +1,19 @@
+<<template>
+  <section>
+    <page-head v-bind:goBack="true" v-bind:showLogin="true"></page-head>
+  </section>
+</template>
+<script>
+  import pageHead from '@/page/common/header'
+  export default{
+    data:function(){
+      return{
+      }
+    },
+    components:{
+      pageHead
+    }
+  }
+</script>
+<style scoped>
+</style>
