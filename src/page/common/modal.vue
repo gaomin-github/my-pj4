dc48e858927bcf3daddf316402c170777211c465
GMfight <374204501@qq.com>
2018-08-16 00:39:43 +0800

    共用插件modal
    页面遮罩

---
+++ src/page/common/modal.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="modal-container">
+    <slot></slot>
+  </section>
+</template>
+<script>
+  export default {
+
+  }
+</script>
+<style scoped>
+  .modal-container{
+    position: fixed;
+    z-index: 999;
+    width:100%;
+    height:100%;
+    background: rgba(0,0,0,0.4);
+  }
+</style>
