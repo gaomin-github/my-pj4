c952a537d1f04ed0a454e0f7a5e3e39beba3c830
GMfight <374204501@qq.com>
2018-08-19 19:52:32 +0800

    滚动返回
    页面滚动返回快捷按钮

---
+++ src/page/plugin/backTopPlugin.vue
@@ -0,0 +1,43 @@
+<template>
+<section v-if="showBackTop" class="back-top">
+  <svg>
+    <use xlink:href="#back-top"></use>
+  </svg>
+</section>
+</template>
+<script>
+  export default {
+    data:function(){
+      return{
+        showBackTop:false,
+      }
+    },
+    mounted:function(){
+      document.addEventListener('scroll',()=>{
+        let scrollY=document.documentElement.scrollTop||document.body.scrollTop
+        let clientY=document.body.clientHeight;
+        if(scrollY>clientY){
+          this.showBackTop=true
+        }
+      })
+    }
+  }
+</script>
+<style scoped lang="scss">
+  @import '../../assets/style/common';
+  .back-top{
+    position:fixed;
+    right:.5rem;
+    bottom:4rem;
+    z-index:2;
+    width:2rem;
+    height:2rem;
+    svg{
+      width:90%;
+      height:90%;
+      transform:rotate(90deg);
+    }
+    border-radius:1rem;
+    background: $blue;
+  }
+</style>
