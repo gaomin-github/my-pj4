06fc24467a11dd3069934c6637af94835b60b4dd
GMfight <374204501@qq.com>
2018-08-25 18:19:45 +0800

    与后台互通
    与后台互通

--- src/config/getApiData.js
+++ src/config/getApiData.js
@@ -8,5 +8,10 @@ export default{
     // console.log('get indexnav list')
     let res=await obj.$http.post('http://localhost:9000/shoplist',{},{emulateJSON:true})
     return res.body
+  },
+  login:async function(obj,data){
+    let res=await obj.$http.post('http://localhost:9000/login',data,{emulateJSON:true})
+    console.log(res.body)
+    return res.body
   }
 }
