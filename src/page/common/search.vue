3e173634fd3393488b2387e70f62861d3536b834
GMfight <374204501@qq.com>
2018-08-30 10:01:03 +0800

    search
    搜索页面

--- src/page/common/search.vue
+++ src/page/common/search.vue
@@ -1,6 +1,29 @@
 <<template>
   <section>
-    <page-head v-bind:goBack="true" v-bind:showLogin="true"></page-head>
+    <page-head class="search-top" v-bind:goBack="true" v-bind:showLogin="true">
+      <section class="search" slot="search">
+        <input type="text" placeholder="搜索商家、商品名称"/>
+      </section>
+      <section class="btn" slot="rightFun">
+        <button>搜索</button>
+      </section>
+    </page-head>
+    <section class="search-container">
+      <section class="search-history">
+        <ul>
+          <li>张三1</li>
+          <li>张三11</li>
+          <li>张三111</li>
+          <li>张三111</li>
+          <li>张三1111</li>
+          <li>张三11111</li>
+          <li>张三111111</li>
+        </ul>
+      </section>
+      <section class="search-result">
+
+      </section>
+    </section>
   </section>
 </template>
 <script>
@@ -15,5 +38,44 @@
     }
   }
 </script>
-<style scoped>
+<style scoped lang="scss">
+  .search-top{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    .search{
+      flex:1;
+      background-color: rgb(255,255,255);
+      border-radius: 1rem;
+      padding:.2rem 1rem;
+      margin:0 1rem;
+      input{
+        width:100%;
+        line-height: 1.5rem;
+        font-size: .8rem;
+      }
+    }
+    .btn{
+      width:3rem;
+      button{
+        font-size: 1rem;
+      }
+    }
+  }
+  .search-container{
+    padding-top: 3rem;
+  }
+  .search-history{
+    ul{
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+
+      li{
+        display: inline-block;
+        border-bottom: 1px rgb(180,180,180) solid;
+        line-height: 2rem;
+      }
+    }
+  }
 </style>
