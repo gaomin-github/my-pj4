0b0c7af9f32e0b0b3c07c600093758fcaa15abbe
GMfight <374204501@qq.com>
2018-08-30 10:04:19 +0800

    路由
    路由总页面

--- src/route/index.js
+++ src/route/index.js
@@ -1,22 +1,43 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
+import AppBak from '@/AppBak'
 import home from '@/page/home'
 import search from '@/page/common/search'
 import login from '@/page/user/login'
+import shop from '@/page/goods/shopDetail'
+import mine from '@/page/user/mine'
+import user from '@/page/user/userInfo'
 export default new VueRouter({
   mode:'history',
   routes:[{
     path:'/',
     redirect:'/home',
+    meta:{
+      title:'首页',
+      keepAlive:true
+    }
   },{
     path:'/home',
     component:home,
+    meta:{
+      title:'首页',
+      keepAlive:true
+    }
   },{
-    path:'/search',
-    component:search,
+    path:'/shop',
+    component:shop
   },{
     path:'/login',
-    component:login
-  }]
+    component:login,
+  },
+    {
+    path:'/user',
+    component:user,
+  },
+    {
+      path:'search',
+      component:search
+    }
+  ]
 })
