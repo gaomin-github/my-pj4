65c2eb3356bfb6e293c7a7630e2b4fb6914a345a
GMfight <374204501@qq.com>
2018-08-30 10:05:10 +0800

    首页
    默认导航页面

--- src/page/home.vue
+++ src/page/home.vue
@@ -1,34 +1,61 @@
 <template>
   <section>
-    <!--<component :is="currentNav"></component>-->
-    <com-shop></com-shop>
-    <page-foot></page-foot>
+    <keep-alive>
+      <component v-bind:is="currentNav"></component>
+    </keep-alive>
+    <!--<com-shop></com-shop>-->
+    <page-foot v-on:changeNav="changeNav"></page-foot>
+    {{test}}
   </section>
 </template>
 <script>
   import pageFoot from '@/page/common/footer'
-  import comShop from '@/page/goods/comShop'
+  import comshop from '@/page/goods/comShop'
+  import search from '@/page/common/search'
+  import mine from '@/page/user/mine'
+//  import comShop from '@/page/goods/comShop'
   export default{
     data:function(){
       return{
-        currentNav:'comShop'
+        currentNav:'comshop'
       }
     },
     components:{
       pageFoot,
-      comShop
+      comshop,
+      search,
+      mine
+    },
+    created:function(){
+//      console.log('created===========')
+//      console.log('home meta:'+this.$route.meta.keepAlive)
+    },
+    activated:function(){
+//      console.log('activated===========')
+    },
+    deactivated:function(){
+//      console.log('deactivated===========')
+    },
+    mounted:function(){
+//      console.log('mounted===========')
+//      console.log('route param:'+this.$route.params.currentNav)
+//      if(this.$route.params.currentNav){
+//        this.currentNav=this.$route.currentNav
+//      }
+    },
+    computed:{
+      test:function(){
+//        console.log('computed===========')
+      }
     },
     methods:{
-     test1(){
-    console.log('33333333')
-  }
+      changeNav(param){
+        this.currentNav=param
+      }
     }
   }
 
 </script>
 <style scoped>
 
-.home{
-  width:100%;
-}
 </style>
