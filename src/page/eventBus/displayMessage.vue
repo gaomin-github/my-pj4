5f1e9a44c3476363c41add49ad85151019e58846
GMfight <374204501@qq.com>
2018-10-10 17:18:54 +0800

    event bus
    event bus自己实现（粗略版）

---
+++ src/page/eventBus/displayMessage.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="display-msg">
+    <button @click="updateMessage">修改信息</button>
+    <h1>{{message}}</h1>
+  </section>
+</template>
+<script>
+  import {eventBus} from '@/page/eventBus/eventBus.js'
+  export default{
+    data:function(){
+      return {
+        message:'999'
+      }
+    },
+    created:function(){
+
+    },
+    mounted:function(){
+      console.log('this.message1:'+this.message)
+      eventBus.$on('update-msg',(newMessage)=>{
+        console.log('newMessage:'+newMessage)
+        this.message=newMessage
+        console.log('this.message2:'+this.message)
+      })
+    },
+    methods:{
+      updateMessage(){
+//        this.$router.push('operatemessage')
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/page/eventBus/operateMessage.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="wp-container">
+    <button @click="updateMg">确定</button>
+    <input class="msg" type="text" v-model="newMessage"/>
+  </section>
+</template>
+<script>
+  import {eventBus} from '@/page/eventBus/eventBus'
+  export default{
+    data:function(){
+      return {
+        newMessage:'',
+      }
+    },
+    methods:{
+      updateMg(){
+        console.log('this.newMessage：'+this.newMessage)
+        eventBus.$emit('update-msg',this.newMessage)
+//        this.$router.push({path:'displaymessage'})
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .msg{
+    border:1px black solid;
+  }
+</style>
