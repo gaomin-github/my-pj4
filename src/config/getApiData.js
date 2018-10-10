9afe13248cfc5d7b6f3e659bd3c839a90a8a0ae7
GMfight <374204501@qq.com>
2018-10-10 17:21:36 +0800

    service data interface
    服务数据操作的统一接口

--- src/config/getApiData.js
+++ src/config/getApiData.js
@@ -13,5 +13,10 @@ export default{
     let res=await obj.$http.post('http://localhost:9000/login',data,{emulateJSON:true})
     console.log(res.body)
     return res.body
+  },
+  columnFall:async function(obj,data){
+    let res=await obj.$http.get('http://localhost:9000/columnfall',{emulateJSON:true})
+    // console.log(res.body)
+    return res.body
   }
 }
